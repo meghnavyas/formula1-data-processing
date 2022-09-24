09b1fbbab3b7d23aa22b969c1fca9382633222e1
Meghna Vyas <55871824+meghnavyas@users.noreply.github.com>
2022-09-25 01:21:50 +0530

    Update: README.md notebooks

---
+++ notebooks/Formula1/ingestion/ingest_circuits_file.py
@@ -0,0 +1,71 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ## Ingest circuits.csv file
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Step 1 - Define a schema for the DataFrame circuits_df
+
+# COMMAND ----------
+
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DoubleType
+
+# COMMAND ----------
+
+circuits_schema = StructType([StructField("circuitId", IntegerType(), False),
+                             StructField("circuitRef", StringType(), True),
+                             StructField("name", StringType(), True),
+                             StructField("location", StringType(), True),
+                             StructField("country", StringType(), True),
+                             StructField("lat", DoubleType(), True),
+                             StructField("lng", DoubleType(), True),
+                             StructField("alt", IntegerType(), True),
+                             StructField("url", StringType(), True)
+                            ])
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Step 2 - Read the circuits.csv file using Spark DataFrame reader with the above defined schema
+
+# COMMAND ----------
+
+circuits_df = spark.read.csv("dbfs:/mnt/2022formula1dl/raw/circuits.csv", header = True, schema = circuits_schema)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Step 3 - Select only required columns and rename the selected columns
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col
+
+# COMMAND ----------
+
+circuits_renamed_df = circuits_df.select(col("circuitId").alias("circuit_id"), col("circuitRef").alias("circuit_ref"), col("name"),
+                                         col("location"), col("country"), col("lat").alias("latitude"), col("lng").alias("longitude"),
+                                         col("alt").alias("altitude"))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Step 4 - Add a new column; ingestion_date to the DataFrame
+
+# COMMAND ----------
+
+from pyspark.sql.functions import current_timestamp
+
+# COMMAND ----------
+
+circuits_final_df = circuits_renamed_df.withColumn("ingestion_date", current_timestamp())
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC ##### Step 5 - Write the DataFrame to a parquet file in ADLS
+
+# COMMAND ----------
+
+circuits_final_df.write.parquet("/mnt/2022formula1dl/processed/circuits", mode = "overwrite")
