e3f1de62e404c9a8ca705fc9ce34889e37b10db0
Meghna Vyas <55871824+meghnavyas@users.noreply.github.com>
2022-09-26 15:07:22 +0530

    added step 2 - 4

--- notebooks/Formula1/ingestion/ingest_constructors_file.py
+++ notebooks/Formula1/ingestion/ingest_constructors_file.py
@@ -17,3 +17,33 @@ constructors_df = spark.read.json("/mnt/2022formula1dl/raw/constructors.json", s
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ##### Step 2 - Drop the unwanted column: url
+
+# COMMAND ----------
+
+constructors_dropped_df = constructors_df.drop('url')
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Step 3 - Rename the columns and add a new column: ingestion_date
+
+# COMMAND ----------
+
+from pyspark.sql.functions import current_timestamp
+
+# COMMAND ----------
+
+constructors_final_df = constructors_dropped_df.withColumnRenamed("constructorId", "constructor_id") \
+                                               .withColumnRenamed("constructorRef", "constructor_ref") \
+                                               .withColumn("ingestion_date", current_timestamp())
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Step 4 - Write the dataframe into a parquet file in ADLS
+
+# COMMAND ----------
+
+constructors_final_df.write.parquet("/mnt/2022formula1dl/processed/constructors", mode = "overwrite")
