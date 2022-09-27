1a3b16248fb1e5f212befd5bf010cb203f87b526
Meghna Vyas <55871824+meghnavyas@users.noreply.github.com>
2022-09-27 12:11:53 +0530

    Initial commit: added raw, processed storage folder paths

---
+++ notebooks/Formula1/includes/configuration.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ##### Includes all the configuration variables. These will be used as variables in other notebooks
+
+# COMMAND ----------
+
+raw_folder_path = "/mnt/2022formula1dl/raw"
+processed_folder_path = "/mnt/2022formula1dl/processed"
