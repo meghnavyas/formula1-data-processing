c4cb3aa9157022129afdb3ea25c842446c6002aa
Meghna Vyas <55871824+meghnavyas@users.noreply.github.com>
2022-09-29 11:53:14 +0530

    added function: read_parquet_file

--- notebooks/Formula1/includes/common_functions.py
+++ notebooks/Formula1/includes/common_functions.py
@@ -8,3 +8,9 @@ from pyspark.sql.functions import current_timestamp
 
 def add_ingestion_date(input_df):
     return input_df.withColumn("ingestion_date", current_timestamp())
+
+# COMMAND ----------
+
+# Function to read given parquet file from given folder
+def read_parquet_file(mount_path, folder):
+    return spark.read.parquet(f"{mount_path}/{folder}")
