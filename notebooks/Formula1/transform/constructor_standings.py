6231f668710d7a5cfe741bc0a034306803403f86
Meghna Vyas <55871824+meghnavyas@users.noreply.github.com>
2022-10-01 17:17:44 +0530

    Update: README.md notebooks

---
+++ notebooks/Formula1/transform/constructor_standings.py
@@ -0,0 +1,52 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ### Constructor Standings
+
+# COMMAND ----------
+
+# MAGIC %run "../includes/configuration"
+
+# COMMAND ----------
+
+# MAGIC %run "../includes/common_functions"
+
+# COMMAND ----------
+
+# Read the race_results parquet files from presentation folder
+race_results_df = read_parquet_file(presentation_folder_path, "race_results")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Creating constructors standings DataFrame having following attributes:
+# MAGIC 1. race_year
+# MAGIC 2. team
+# MAGIC 3. wins
+# MAGIC 4. points
+# MAGIC 5. rank
+
+# COMMAND ----------
+
+from pyspark.sql.functions import sum, count, when, col
+
+# COMMAND ----------
+
+# Adding wins and points columns
+constructor_standings_df = race_results_df.groupBy("race_year", "team") \
+                                       .agg(sum("points").alias("points"), count(when(col("position") == 1, True)).alias("wins"))
+
+# COMMAND ----------
+
+from pyspark.sql import Window
+from pyspark.sql.functions import rank, desc
+
+# COMMAND ----------
+
+# Add rank column based on wins and points
+rank_window = Window.partitionBy("race_year").orderBy(desc("wins"), desc("points"))
+final_df = constructor_standings_df.withColumn("rank", rank().over(rank_window))
+
+# COMMAND ----------
+
+# Write it into presentation layer in parquet format
+final_df.write.parquet(f"{presentation_folder_path}/constructor_standings", mode = "overwrite")
