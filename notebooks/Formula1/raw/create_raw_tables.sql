18f8ddf2f288d0644a79eaa33724af3bb51c96a2
Meghna Vyas <55871824+meghnavyas@users.noreply.github.com>
2022-10-02 11:35:15 +0530

    added create statement for races table

--- notebooks/Formula1/raw/create_raw_tables.sql
+++ notebooks/Formula1/raw/create_raw_tables.sql
@@ -3,10 +3,6 @@ CREATE DATABASE IF NOT EXISTS f1_raw;
 
 -- COMMAND ----------
 
-SELECT CURRENT_DATABASE();
-
--- COMMAND ----------
-
 USE f1_raw;
 
 -- COMMAND ----------
@@ -35,7 +31,30 @@ OPTIONS (path "/mnt/2022formula1dl/raw/circuits.csv", header true);
 
 -- COMMAND ----------
 
-Select* from f1_raw.circuits;
+-- MAGIC %md
+-- MAGIC #### Create races table
+
+-- COMMAND ----------
+
+DROP TABLE IF EXISTS f1_raw.races;
+
+CREATE TABLE IF NOT EXISTS f1_raw.races
+(
+  raceId INT,
+  year INT,
+  round INT,
+  circuitId INT,
+  name STRING,
+  date DATE,
+  time STRING,
+  url STRING
+)
+USING csv
+OPTIONS (path "/mnt/2022formula1dl/raw/races.csv", header true);
+
+-- COMMAND ----------
+
+Select* from f1_raw.races;
 
 -- COMMAND ----------
 
