1a01b838fe9d8641d6b9ed96adcbcc214ef7b2a4
Meghna Vyas <55871824+meghnavyas@users.noreply.github.com>
2022-10-05 20:58:10 +0530

    added code to support incremental load

--- notebooks/Formula1/ingestion/ingest_pit_stops_file.py
+++ notebooks/Formula1/ingestion/ingest_pit_stops_file.py
@@ -9,6 +9,11 @@ v_data_source = dbutils.widgets.get("p_data_source")
 
 # COMMAND ----------
 
+dbutils.widgets.text("p_file_date", "")
+v_file_date = dbutils.widgets.get("p_file_date")
+
+# COMMAND ----------
+
 # MAGIC %run "../includes/configuration"
 
 # COMMAND ----------
@@ -37,12 +42,12 @@ pitstops_schema = StructType([StructField("driverId", IntegerType(), False),
 
 # COMMAND ----------
 
-pitstops_df = spark.read.json(f"{raw_folder_path}/pit_stops.json", multiLine = True, schema = pitstops_schema)
+pitstops_df = spark.read.json(f"{raw_folder_path}/{v_file_date}/pit_stops.json", multiLine = True, schema = pitstops_schema)
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ##### Step 2 - Rename the columns and add a new column: ingestion_date
+# MAGIC ##### Step 2 - Rename the columns and add new columns: data_source, file_date, ingestion_date
 
 # COMMAND ----------
 
@@ -52,7 +57,8 @@ from pyspark.sql.functions import current_timestamp, lit
 
 pitstops_renamed_df = pitstops_df.withColumnRenamed("driverId", "driver_id") \
                                  .withColumnRenamed("raceId", "race_id") \
-                                 .withColumn("data_source", lit(v_data_source))
+                                 .withColumn("data_source", lit(v_data_source)) \
+                                 .withColumn("file_date", lit(v_file_date))
 
 # COMMAND ----------
 
@@ -65,8 +71,19 @@ pitstops_final_df = add_ingestion_date(pitstops_renamed_df)
 
 # COMMAND ----------
 
-pitstops_final_df.write.mode("overwrite").format("parquet").saveAsTable("f1_processed.pit_stops")
+overwrite_partition(pitstops_final_df, "f1_processed", "pit_stops", "race_id")
 
 # COMMAND ----------
 
 dbutils.notebook.exit("Success")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC Select race_id, count(1)
+# MAGIC   FROM f1_processed.pit_stops
+# MAGIC   GROUP BY 1
+# MAGIC   ORDER BY 1 DESC;
+
+# COMMAND ----------
+
