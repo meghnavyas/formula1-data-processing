84166297fadf5f81edbbe62752e5fbea563a66e8
Meghna Vyas <55871824+meghnavyas@users.noreply.github.com>
2022-10-07 18:01:04 +0530

    changed write target to delta table

--- notebooks/Formula1/ingestion/ingest_drivers_file.py
+++ notebooks/Formula1/ingestion/ingest_drivers_file.py
@@ -99,7 +99,7 @@ drivers_final_df = drivers_ingested_df.drop("url")
 
 # COMMAND ----------
 
-drivers_final_df.write.mode("overwrite").format("parquet").saveAsTable("f1_processed.drivers")
+drivers_final_df.write.mode("overwrite").format("delta").saveAsTable("f1_processed.drivers")
 
 # COMMAND ----------
 
